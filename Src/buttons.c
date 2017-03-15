bf94d28e3e03a303b88b656b62044025e8d40500
jsjolund <j.sjolund@gmail.com>
2017-03-15 22:02:56 +0100

    Buttons code more readable

--- Src/buttons.c
+++ Src/buttons.c
@@ -27,23 +27,30 @@ static I2C_HandleTypeDef *btnHi2c;
 #define IOEXP_ADDRESS_F (IOEXP_ADDRESS + 0)
 #define IOEXP_ADDRESS_S (IOEXP_ADDRESS + 2)
 
+void Button_I2C_Read(uint16_t DevAddress, uint16_t MemAddress, uint8_t *pData, uint16_t Size) {
+	while (HAL_I2C_Mem_Read(btnHi2c, DevAddress, MemAddress, I2C_MEMADD_SIZE_8BIT, pData, Size, BUTTONS_I2C_TIMEOUT) != HAL_OK)
+		;
+}
+
+void Button_I2C_Write(uint16_t DevAddress, uint16_t MemAddress, uint8_t *pData, uint16_t Size) {
+	while (HAL_I2C_Mem_Write(btnHi2c, DevAddress, MemAddress, I2C_MEMADD_SIZE_8BIT, pData, Size, BUTTONS_I2C_TIMEOUT) != HAL_OK)
+		;
+}
+
 void ButtonSetLED(int devAddress, int btnPin, uint8_t state) {
 	uint16_t gpioState;
-	while (HAL_I2C_Mem_Read(btnHi2c, devAddress, IOEXP_GPIOA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &gpioState, sizeof(gpioState), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-		;
+	Button_I2C_Read(devAddress, IOEXP_GPIOA, (uint8_t *) &gpioState, sizeof(gpioState));
 	if (state)
 		gpioState |= (btnPin >> 1);
 	else
 		gpioState &= ~(btnPin >> 1);
-	while (HAL_I2C_Mem_Write(btnHi2c, devAddress, IOEXP_GPIOA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &gpioState, sizeof(gpioState), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-		;
+	Button_I2C_Write(devAddress, IOEXP_GPIOA, (uint8_t *) &gpioState, sizeof(gpioState));
 }
 
 uint8_t ButtonGetLED(int devAddress, int btnPin) {
-	uint16_t status;
-	while (HAL_I2C_Mem_Read(btnHi2c, devAddress, IOEXP_GPIOA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &status, sizeof(status), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-		;
-	return (status & (btnPin >> 1)) ? 1 : 0;
+	uint16_t gpioState;
+	Button_I2C_Read(devAddress, IOEXP_GPIOA, (uint8_t *) &gpioState, sizeof(gpioState));
+	return (gpioState & (btnPin >> 1)) ? 1 : 0;
 }
 
 void ButtonEvent(int devAddress, int btnPin, uint8_t isPressed) {
@@ -131,14 +138,11 @@ void EXTI9_5_IRQHandler(void) {
 
 	if (HAL_GPIO_ReadPin(B2_GPIO_Port, B2_Pin) == GPIO_PIN_SET) {
 
-		uint16_t flags;
-		uint16_t state;
+		uint16_t flags, state;
 		uint8_t isPressed;
 
-		while (HAL_I2C_Mem_Read(btnHi2c, IOEXP_ADDRESS_F, INTFA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &flags, sizeof(flags), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-			;
-		while (HAL_I2C_Mem_Read(btnHi2c, IOEXP_ADDRESS_F, IOEXP_GPIOA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &state, sizeof(state), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-			;
+		Button_I2C_Read(IOEXP_ADDRESS_F, INTFA, (uint8_t *) &flags, sizeof(flags));
+		Button_I2C_Read(IOEXP_ADDRESS_F, IOEXP_GPIOA, (uint8_t *) &state, sizeof(state));
 		for (i = 0; i < NUM_BUTTONS_F; i++) {
 			if (ButtonChangedState(BUTTONS_F[i], flags, state, &isPressed)) {
 				ButtonEvent(IOEXP_ADDRESS_F, BUTTONS_F[i], isPressed);
@@ -149,10 +153,8 @@ void EXTI9_5_IRQHandler(void) {
 		state = 0;
 		isPressed = 0;
 
-		while (HAL_I2C_Mem_Read(btnHi2c, IOEXP_ADDRESS_S, INTFA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &flags, sizeof(flags), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-			;
-		while (HAL_I2C_Mem_Read(btnHi2c, IOEXP_ADDRESS_S, IOEXP_GPIOA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &state, sizeof(state), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-			;
+		Button_I2C_Read(IOEXP_ADDRESS_S, INTFA, (uint8_t *) &flags, sizeof(flags));
+		Button_I2C_Read(IOEXP_ADDRESS_S, IOEXP_GPIOA, (uint8_t *) &state, sizeof(state));
 		for (i = 0; i < NUM_BUTTONS_S; i++) {
 			if (ButtonChangedState(BUTTONS_S[i], flags, state, &isPressed)) {
 				ButtonEvent(IOEXP_ADDRESS_S, BUTTONS_S[i], isPressed);
@@ -160,8 +162,10 @@ void EXTI9_5_IRQHandler(void) {
 		}
 	}
 	EXTI->PR |= B2_Pin;
-	while (HAL_I2C_IsDeviceReady(btnHi2c, IOEXP_ADDRESS_F, 100, BUTTONS_I2C_TIMEOUT) != HAL_OK);
-	while (HAL_I2C_IsDeviceReady(btnHi2c, IOEXP_ADDRESS_S, 100, BUTTONS_I2C_TIMEOUT) != HAL_OK);
+	while (HAL_I2C_IsDeviceReady(btnHi2c, IOEXP_ADDRESS_F, 100, BUTTONS_I2C_TIMEOUT) != HAL_OK)
+		;
+	while (HAL_I2C_IsDeviceReady(btnHi2c, IOEXP_ADDRESS_S, 100, BUTTONS_I2C_TIMEOUT) != HAL_OK)
+		;
 	if (HAL_GPIO_ReadPin(B2_GPIO_Port, B2_Pin) == GPIO_PIN_SET) {
 		EXTI9_5_IRQHandler();
 	}
@@ -172,31 +176,29 @@ void ButtonsInitIOexpander(uint16_t devAddress) {
 
 	// Byte mode with IOCON.BANK = 0, INT pins internally connected, INT active-high
 	uint8_t settings = IOCON_MIRROR | IOCON_INTPOL;
-	while (HAL_I2C_Mem_Write(btnHi2c, devAddress, IOCONA, I2C_MEMADD_SIZE_8BIT, &settings, sizeof(settings), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-		;
-	while (HAL_I2C_Mem_Write(btnHi2c, devAddress, IOCONB, I2C_MEMADD_SIZE_8BIT, &settings, sizeof(settings), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-		;
+	Button_I2C_Write(devAddress, IOCONA, &settings, sizeof(settings));
+	Button_I2C_Write(devAddress, IOCONB, &settings, sizeof(settings));
+
 	uint16_t direction = 0b1010101010101010; // Each even pin is output, odd inputs (1 = Pin is configured as an input)
-	while (HAL_I2C_Mem_Write(btnHi2c, devAddress, IODIRA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &direction, sizeof(direction), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-		;
+	Button_I2C_Write(devAddress, IODIRA, (uint8_t *) &direction, sizeof(direction));
+
 	// 1 = Enable GPIO input pin for interrupt-on-change event.
-	while (HAL_I2C_Mem_Write(btnHi2c, devAddress, GPINTENA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &direction, sizeof(direction), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-		;
+	Button_I2C_Write(devAddress, GPINTENA, (uint8_t *) &direction, sizeof(direction));
+
 	// Interrupt on change for inputs. 0 = Pin value is compared against the previous pin value.
 	uint16_t intcon = 0;
-	while (HAL_I2C_Mem_Write(btnHi2c, devAddress, INTCONA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &intcon, sizeof(intcon), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-		;
+	Button_I2C_Write(devAddress, INTCONA, (uint8_t *) &intcon, sizeof(intcon));
+
 	// 1 = Pull-up enabled
 	uint16_t pullups = 0xFFFF;
-	while (HAL_I2C_Mem_Write(btnHi2c, devAddress, GPPUA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &pullups, sizeof(pullups), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-		;
+	Button_I2C_Write(devAddress, GPPUA, (uint8_t *) &pullups, sizeof(pullups));
+
 	uint16_t levels = 0;
-	while (HAL_I2C_Mem_Write(btnHi2c, devAddress, IOEXP_GPIOA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &levels, sizeof(levels), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-		;
+	Button_I2C_Write(devAddress, IOEXP_GPIOA, (uint8_t *) &levels, sizeof(levels));
+
 	// Read once to reset any interrupt related stuff
 	volatile uint16_t result;
-	while (HAL_I2C_Mem_Read(btnHi2c, devAddress, IOEXP_GPIOA, I2C_MEMADD_SIZE_8BIT, (uint8_t *) &result, sizeof(result), BUTTONS_I2C_TIMEOUT) != HAL_OK)
-		;
+	Button_I2C_Read(devAddress, IOEXP_GPIOA, (uint8_t *) &result, sizeof(result));
 }
 
 void ButtonsInit(I2C_HandleTypeDef *hi2cHandle) {
